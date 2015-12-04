291b3e51fcd07cda74bae22e154bb2406a234201
jeffward01 <jeffwardcoding@gmail.com>
2015-12-03 21:01:55 -0800

    reviewPost Domain class completed

--- ReviewApplicaiton/ReviewApplication.CORE/Domain/ReviewPost.cs
+++ ReviewApplicaiton/ReviewApplication.CORE/Domain/ReviewPost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReviewApplication.CORE.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,27 @@ namespace ReviewApplication.CORE.Domain
         public int ReviewPostID { get; set; } //Primary Key
         public int CompanyID { get; set; } // Foriegn Key
         public int InsuranceAgentID { get; set; } // Foriegn Key
+        public DateTime ReviewPostDate { get; set; }
 
-        public int CompanyRating { get; set; }
+        public int? CompanyRating { get; set; }
+        public int? AgentRating { get; set; }
         public string PostTitle { get; set; }
         public string PostBody { get; set; }
         public int NumberOfLikes { get; set; }
 
         public virtual ICollection<Comment> Comments { get; set; }
+
+        public void Update(ReviewPostModel reviewPost)
+        {
+            ReviewPostID = reviewPost.ReviewPostID;
+            CompanyID = reviewPost.CompanyID;
+            InsuranceAgentID = reviewPost.InsuranceAgentID;
+            ReviewPostDate = reviewPost.ReviewPostDate;
+            CompanyRating = reviewPost.CompanyRating;
+            AgentRating = reviewPost.AgentRating;
+            PostBody = reviewPost.PostBody;
+            PostTitle = reviewPost.PostTitle;
+            NumberOfLikes = reviewPost.NumberOfLikes;
+        }
     }
 }
--- ReviewApplicaiton/ReviewApplication.CORE/Models/ReviewPostModel.cs
+++ ReviewApplicaiton/ReviewApplication.CORE/Models/ReviewPostModel.cs
@@ -11,15 +11,17 @@ namespace ReviewApplication.CORE.Models
         public int ReviewPostID { get; set; } //Primary Key
         public int CompanyID { get; set; } // Foriegn Key
         public int InsuranceAgentID { get; set; } // Foriegn Key
+        public DateTime ReviewPostDate { get; set; }
 
-        public int CompanyRating { get; set; }
+        public int? CompanyRating { get; set; }
+        public int? AgentRating { get; set; }
         public string PostTitle { get; set; }
         public string PostBody { get; set; }
-        public string NumberOfLikes { get; set; }
+        public int NumberOfLikes { get; set; }
 
         public CompanyProfileModel Company { get; set; }
         public InsuranceProfileAgentModel InsuranceAgent { get; set; }
 
-        public CommentModel Comments { get; set; }
+        public CommentModel Comment { get; set; }
     }
 }
